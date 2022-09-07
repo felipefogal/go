cbd85a89d256afd117a79d408ee961102dba571c
Felipe Garcia Fogal <felipefogal@gmail.com>
2022-09-06 22:38:44 -0300

    Continuando estudos Golang

--- felipe/banco/main.go
+++ felipe/banco/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/felipe/banco/clientes"
+	"github.com/felipe/banco/contas"
+)
+
+func main() {
+	contaDoBruno := contas.ContaCorrente{Titular: clientes.Titular{
+		Nome:      "Bruno",
+		CPF:       "123.123.123.12",
+		Profissao: "Desenvolvedor"},
+		NumeroAgencia: 123, NumeroConta: 123456, Saldo: 100}
+
+	fmt.Println(contaDoBruno)
 }
